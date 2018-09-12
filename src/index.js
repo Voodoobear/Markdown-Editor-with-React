c451b1a7baf9500c32e18b2939261654cfb5b0e5
Voodoobear <s.chalain@gmail.com>
2018-09-12 02:04:04 +0200

    préparation de base : éditeur : rendu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-// Ici on code notre App
+import React from 'react';
+import { render } from 'react-dom';
+import './style/css/bootstrap.min.css';
+import './index.css';
+import { sampleText } from './sampleText';
+
+class App extends React.Component {
+
+    state = {
+        text: sampleText
+    };
+
+    editText = (event) => {
+        const text = event.target.value;
+        this.setState({ text });
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-sm-6">
+                        <textarea 
+                        rows="35" 
+                        className="form-control" 
+                        value={this.state.text}
+                        onChange={(e) => this.editText(e)}
+                        >
+
+                        </textarea>
+                    </div>
+
+                    <div className="col-sm-6">
+                        <h1>Résultats</h1>
+                    </div>
+                    {sampleText}
+                </div>
+            </div>
+        )
+    }
+}
+
+render(
+    <App />,
+    document.getElementById('root') 
+);
